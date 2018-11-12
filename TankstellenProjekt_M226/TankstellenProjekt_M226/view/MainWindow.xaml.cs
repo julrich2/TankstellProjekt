12fac34b8fd785e1b74fc00dd6389a5fd2016871
julrich2 <44360162+julrich2@users.noreply.github.com>
2018-11-12 09:14:33 +0100

    asdfghjklöä$

--- TankstellenProjekt_M226/TankstellenProjekt_M226/view/MainWindow.xaml.cs
+++ TankstellenProjekt_M226/TankstellenProjekt_M226/view/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using TankstellenProjekt_M226.Controllers;
 
 namespace TankstellenProjekt_M226
 {
