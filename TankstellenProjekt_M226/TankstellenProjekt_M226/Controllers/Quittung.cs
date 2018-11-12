6bd13ff8a7eb17a79c81a34f700a8087a042f802
julrich2 <44360162+julrich2@users.noreply.github.com>
2018-11-12 10:29:52 +0100

    wrefgdb

--- TankstellenProjekt_M226/TankstellenProjekt_M226/Controllers/Quittung.cs
+++ TankstellenProjekt_M226/TankstellenProjekt_M226/Controllers/Quittung.cs
@@ -8,10 +8,18 @@ namespace TankstellenProjekt_M226.Controllers
 {
     class Quittung
     {
-        private string Treibstoff;
-        private string Literzahl;
-        private double Betrag;
-        private string Datum;
-        private string Uhrzeit;
+        private string treibstoff;
+        private string literzahl;
+        private double betrag;
+        private string datum;
+        private string uhrzeit;
+
+        public string Treibstoff { get; set; }
+        public string Literzahl { get; set; }
+        public double Betrag { get; set; }
+        public string Uhrzeit { get; set; }
+        public string Datum { get; set; }
     }
+
+
 }
