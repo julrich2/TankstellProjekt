bd113f40df1c43335a04a639bfd8919bca4f20e9
julrich2 <44360162+julrich2@users.noreply.github.com>
2018-11-12 09:12:28 +0100

    umgestaltung

--- TankstellenProjekt_M226/TankstellenProjekt_M226/view/MainWindow.xaml.cs
+++ TankstellenProjekt_M226/TankstellenProjekt_M226/view/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TankstellenProjekt_M226.Controllers;
 
 namespace TankstellenProjekt_M226
 {
@@ -23,6 +24,26 @@ namespace TankstellenProjekt_M226
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnSPlus_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnpLus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnBenzin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDiesel_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
