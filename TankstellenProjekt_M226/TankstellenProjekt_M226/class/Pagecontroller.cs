2dd9c6ff7a5cfc3e17abc1303b35e920d63c0409
julrich2 <44360162+julrich2@users.noreply.github.com>
2018-11-12 08:57:27 +0100

    pagecontroller

---
+++ TankstellenProjekt_M226/TankstellenProjekt_M226/class/Pagecontroller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _00_trainingApp.Controller
+{
+    public class Pagecontroller
+    {
+        public static void NextWindow(Window startwindow, Window targetwindow)
+        {
+            targetwindow.Show();
+            startwindow.Close();
+
+        }
+    }
+}
