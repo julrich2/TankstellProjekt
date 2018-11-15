83969886bb4490f71024679401dd37dbe7481506
julrich2 <44360162+julrich2@users.noreply.github.com>
2018-11-15 09:05:57 +0100

    kasse fast fertig

--- TankstellenProjekt_M226/TankstellenProjekt_M226/view/Kasse.xaml.cs
+++ TankstellenProjekt_M226/TankstellenProjekt_M226/view/Kasse.xaml.cs
@@ -23,5 +23,71 @@ namespace TankstellenProjekt_M226.view
         {
             InitializeComponent();
         }
+
+        void AddMoney(double Money)
+        {
+            double paid;
+            string result="0";
+            paid=Convert.ToDouble(Bezahlt.Content); 
+            paid=paid+Money;
+            paid = Math.Round(paid, 2);
+            
+            Bezahlt.Content = Convert.ToString(paid) ;
+            
+        }
+
+        private void Btn100_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(100);
+        }
+
+        private void Btn50_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(50);
+        }
+        private void Btn20_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(20);
+        }
+
+        private void Btn10_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(10);
+        }
+
+        private void Btn5_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(5);
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(2);
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(1);
+        }
+
+        private void Btn05_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(0.50);
+        }
+
+        private void Btn02_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(0.20);
+        }
+
+        private void Btn01_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(0.10);
+        }
+
+        private void Btn005_Click(object sender, RoutedEventArgs e)
+        {
+            AddMoney(0.05);
+        }
     }
 }
